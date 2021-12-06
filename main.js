bfbe396816c985c53112200d5c779b2a7b076671
tstepanova <tstepanova@griddynamics.com>
2021-12-06 13:54:46 +0200

    lesson 6 - events

--- main.js
+++ main.js
@@ -8,10 +8,18 @@ const app = Vue.createApp({
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpeg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpeg' }
             ],
-            sizes: ['S', 'M', 'L', 'XL']
+            cart: 0,
         }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateImage(varianImage) {
+            this.image = varianImage;
+        },
     }
 })
